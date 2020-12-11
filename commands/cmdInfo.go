e8854a25ad7dd975418c41c5c8a6960aaefdd1b7
Maia <zgovorovski@gmail.com>
2020-12-11 17:05:41 +0000

    Updated info command

--- commands/cmdInfo.go
+++ commands/cmdInfo.go
@@ -5,5 +5,16 @@ import (
 )
 
 func cmdInfo(e *interaction.Event) *interaction.EventCallback {
-	return e.Reply("Testing bot to test Discord's new slash commands. Operated by Maia")
+	return e.ReplyEmbed(interaction.Embed{
+		Description: "Slashy is an [open-source](https://github.com/maiacodes/slashy) slash commands bot written in Golang!",
+		Color:       "16762655",
+		Author: interaction.EmbedAuthor{
+			Name:    "About Slashy",
+			IconURL: "https://media.discordapp.net/attachments/733250810707705877/762359646828363826/swifty.png?width=128&height=128",
+			URL:     "https://slashy.maia.codes",
+		},
+		Footer: interaction.EmbedFooter{
+			Text: "Made with ❤️ by Maia & Contributors",
+		},
+	})
 }
